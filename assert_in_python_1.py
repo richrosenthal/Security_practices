cd49e8d781900cd54936d0a97872b9943403017f
richrosenthal <rrosenthal2013@gmail.com>
2025-12-03 20:27:59 -0500

    cleans up code a little bit

--- assert_in_python_1.py
+++ assert_in_python_1.py
@@ -1,12 +1,11 @@
 def process_grades():
-    line = input()
-    parts = line.split(',')
+    user_input = input()
+    entered_grades = user_input.split(',')
     grades = []
     total = 0
-    for part in parts:
-        grades.append(int(part))
-        total += int(part)
-        
+    for grade in entered_grades:
+        grades.append(int(grade))
+        total += int(grade)
         
     try:
         assert all(0 <= g <= 100 for g in grades), "All values need to be greater than 0 but less than 100"
@@ -14,10 +13,9 @@ def process_grades():
         print(total)
     except AssertionError as e:
         print(e)
-    
+
 if __name__ == '__main__':
     process_grades()
-
 
 # Write a program that will accept four grades and place them in a list. The sum of the 4 grades should be less or equal to 100. Each individual grade should be greater or equal to 0, but less or equal to 100. Include an assert statement that will display a message if any of these conditions are not met. The assert statements should display the messages "All values need to be greater than 0 but less than 100" and "One of the grades is wrong, total needs to be less than 100!", as appropriate.
 
